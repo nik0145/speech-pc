6957edcd434962094dba21718409db241833c31d
Nikolay Tyurin <kolya_tyurin@mail.ru>
2021-01-18 00:30:56 +0300

    change README and add keyword

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # speech-pc
-из за разных версий пришлось полный путь прописывать к SpeechRecognition
-для установки среды sudo apt install -y python3-venv
-
-pip3 install SpeechRecognition
-pip3 install gTTS
-sudo apt-get install portaudio19-dev
-pip3 install PyAudio
-запуск python3 ./my_env/include/index.py 
-
-шел по этому туториалу https://www.digitalocean.com/community/tutorials/how-to-install-python-3-and-set-up-a-programming-environment-on-an-ubuntu-18-04-server-ru
 python voice assistant
-крутая статья https://habr.com/ru/post/529590/
-нужно запускать от имени рута и сделать конфиг
+
+
+## Install
+```sudo apt install -y python3-venv```<br>
+```pip3 install SpeechRecognition```<br>
+```pip3 install gTTS```<br>
+```sudo apt-get install portaudio19-dev```<br>
+```pip3 install PyAudio```<br>
+```git clone ...```<br>
+## Start
+```python3 ./my_env/include/index.py ```
+# INFO
+[Article](https://www.digitalocean.com/community/tutorials/how-to-install-python-3-and-set-up-a-programming-environment-on-an-ubuntu-18-04-server-ru)
--- my_env/include/index.py
+++ my_env/include/index.py
@@ -6,31 +6,45 @@ import os
 import sys
 import webbrowser
 
+
 def talk(word):
     print(word)
     os.system("spd-say \""+ word+"\"")
-talk("I listen to you master")
-def onStart():
+talk("launch successful")
+def onReturnWord():
     recognizer = sr.Recognizer()
     recognizer.energy_threshold=4000
     with sr.Microphone() as source:
-        
         recognizer.adjust_for_ambient_noise(source, duration=1)
         try:
-            audio = recognizer.listen(source,phrase_time_limit=2)
+            audio = recognizer.listen(source,phrase_time_limit=1)
         except sr.WaitTimeoutError:
-            talk("I do not understand you")
-            words = onStart()
+            talk("Time is over")
+            word = onReturnWord()
     try:
-        words = recognizer.recognize_google(audio, language = "ru-RU").lower()
+        word = recognizer.recognize_google(audio, language = "ru-RU").lower()
     except sr.UnknownValueError:
         talk("I do not understand you")
-        words = onStart()
-    return words
+        word = onReturnWord()
+    return word
+def onStart():
+    print("Listening")
+    global isTalkPhrase
+    print(isTalkPhrase)
+    if isTalkPhrase == True:
+        value = onReturnWord()
+        onCheckPhrase(value)
+        isTalkPhrase = False
+    else:
+        value = onReturnWord()
+        if 'приём' in value:
+            talk("yes ?")
+            isTalkPhrase = True
+            print(isTalkPhrase)
+
 
 def onCheckPhrase(value):
     # сделать из этого конфиг
-    print("вы сказали",value)
     if 'включи музыку' in value:
         url = 'https://www.youtube.com/watch?v=5qap5aO4i9A'
         webbrowser.open(url)
@@ -40,13 +54,13 @@ def onCheckPhrase(value):
         os.system("firefox")
     elif 'выключи компьютер' in value:    
         os.system("sudo shutdown -h now")
-       
+        
     elif 'запустить tracker' in value:  
         #  os.system("gnome-terminal --e \"bash -c \"sudo /usr/share/AppStaff/AppStaff.sh ; exec bash\"\"")
         os.system("gnome-terminal -- bash -c \"sudo /usr/share/AppStaff/AppStaff.sh; exec bash\"")
     elif 'привет' in value:    
         talk("hi")
         
-
+isTalkPhrase = False
 while True:
-    onCheckPhrase(onStart())
+    onStart()
